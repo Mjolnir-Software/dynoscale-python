20dfd7dd79f3e71ecf153d1847246f6c742a5d27
fuho <ondrej.dolejsi@gmail.com>
2022-05-12 21:42:35 +0200

    gunicorn_hooks_overload.conf.py now uses PORT and WEB_CONCURRENCY from env if available.

--- examples/flask_simple/gunicorn_hooks_overload.conf.py
+++ examples/flask_simple/gunicorn_hooks_overload.conf.py
@@ -1,9 +1,13 @@
+import os
+
 from dynoscale.hooks.gunicorn import pre_request as hook  # noqa # pylint: disable=unused-import
 
-bind = "127.0.0.1:3000"
+PORT = int(os.getenv('PORT', '3000'))
+WORKERS = int(os.getenv('WEB_CONCURRENCY', '3'))
+bind = f"0.0.0.0:{PORT}"
 max_requests = 10
 max_requests_jitter = 3
-workers = 3
+workers = WORKERS
 wsgi_app = "examples.flask_simple.flask_simple:app"
 
 
